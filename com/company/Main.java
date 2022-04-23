31860b6282600e9abe8ff2f556bf96bb421bce91
daann148 <danielwarhammer40k@gmail.com>
2022-04-23 09:01:14 +0200

    green commit

--- com/company/Main.java
+++ com/company/Main.java
@@ -3,4 +3,12 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
     }
+    public static int max(int a, int b) {
+            if(a<b){
+                return b;
+            } else if(a>b){
+                return a;
+            }
+            return a;
+    }
 }
