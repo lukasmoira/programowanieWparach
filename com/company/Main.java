57b11236b3afad3a3998fc823cdc5599d82047a1
Patryk <gosikpatryk@gmail.com>
2022-04-23 09:06:56 +0200

    REFACTOR commit

--- com/company/Main.java
+++ com/company/Main.java
@@ -4,11 +4,9 @@ public class Main {
     public static void main(String[] args) {
     }
     public static int max(int a, int b) {
-            if(a<b){
-                return b;
-            } else if(a>b){
-                return a;
-            }
-            return a;
+        if (a < b) {
+            return b;
+        }
+        return a;
     }
 }
